a6436bdabbcdcaa9a184049244053508f825cc90
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-07-21 19:14:27 -0600

    Trying to get this to work

--- Assets/Features/player-input-and-character/Player.cs
+++ Assets/Features/player-input-and-character/Player.cs
@@ -7,22 +7,23 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     private CharacterController _controller;
-    private Vector3 _velocity;
+    private SmoothNumbers<Vector3> _velocity;
     public float speed;
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+        _velocity = new SmoothNumbers<Vector3>(Vector3.zero, 2, new Vector3MoveTowards());
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        _controller.Move(_velocity * Time.deltaTime);
-        Debug.Log(_velocity);
+        _controller.Move(_velocity.CurrentNum * Time.deltaTime);
+        Debug.Log(_velocity.CurrentNum);
     }
     public void MoveControl(InputAction.CallbackContext context)
     {
         Vector2 dir = context.ReadValue<Vector2>();
-        _velocity = new Vector3(dir.x, 0, dir.y) * speed;
+        _velocity.DesiredNum = new Vector3(dir.x, 0, dir.y) * speed;
     }
 }
